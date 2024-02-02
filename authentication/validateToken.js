6e9ad30363529b0a2e737f96e5f017e68a7cac8e
Miisa Suorsa <miisasuorsa@gmail.com>
2024-02-02 13:18:34 +0200

    Update validateToken.js

--- authentication/validateToken.js
+++ authentication/validateToken.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function(req, res, next){
     console.log(req.headers);
+    //get token from headers
     const authHeader = req.headers['authorization'];
     console.log(authHeader);
     let token;
@@ -13,6 +14,7 @@ module.exports = function(req, res, next){
     }
     if (token == null) return res.sendStatus(401);
     console.log("token found");
+    //verify given token to decode it, get user who is validated with the token
     jwt.verify(token, process.env.SECRET, (err, user) => {
         if(err) return res.sendStatus(403);
         req.user=user;
